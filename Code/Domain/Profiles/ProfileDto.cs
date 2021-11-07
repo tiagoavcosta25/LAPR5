22e0e5650e492234f28cfee059f5507e5c7b047f
1190967 <1190967@isep.ipp.pt>
2021-11-07 17:15:27 +0000

    LAPR5-22 #implementation #developing Profile DTO Development

---
+++ Code/Domain/Profiles/ProfileDto.cs
@@ -0,0 +1,24 @@
+using System;
+using DDDSample1.Domain.Categories;
+
+namespace DDDSample1.Domain.Profiles
+{
+    public class ProfileDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get;  set; }
+        public string Name { get;  set; }
+        public string PhoneNumber { get;  set; }
+        public DateTime DateOfBirth { get;  set; }
+
+        public ProfileDto(Guid Id, string name, string email, double phoneNumber, int year, int month, int day)
+        {
+            this.Id = Id;
+            this.Name = name;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+            this.DateOfBirth = new DateTime(year, month, day);
+            this.CategoryId = catId;
+        }
+    }
+}
