22caf9bf18f2748b0fc39de3c4354a5a06865023
1191460 <1191460@isep.ipp.pt>
2021-11-07 22:15:41 +0000

    LAPR5-32 #implementation #developing Connection class generated

---
+++ Code/Domain/Connections/Connection.cs
@@ -0,0 +1,29 @@
+﻿using Code.Domain.Connections;
+using DDDSample1.Domain.Shared;
+
+namespace DDDNetCore.Domain.Connections
+{
+    public class Connection : Entity<ConnectionId>, IAggregateRoot
+    {
+
+        public ConnectionStrength connectionStrength { get; private set; }
+
+        public bool Active { get; private set; }
+
+        private Connection()
+        {
+            this.Active = true;
+        }
+
+        public Connection(int connectionStrength)
+        {
+            this.connectionStrength = new ConnectionStrength(connectionStrength);
+        }
+
+        public void MarkAsInative()
+        {
+            this.Active = false;
+        }
+
+    }
+}
