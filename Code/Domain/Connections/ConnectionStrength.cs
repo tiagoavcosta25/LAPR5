c422a3d934e82bdd178e28f1e9c23a33b907afd7
1191460 <1191460@isep.ipp.pt>
2021-11-07 22:13:43 +0000

    LAPR5-32 #implementation #developing ConnectionStrength class generated

---
+++ Code/Domain/Connections/ConnectionStrength.cs
@@ -0,0 +1,28 @@
+﻿using DDDSample1.Domain.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDNetCore.Domain.Connections
+{
+    [ComplexType]
+    public class ConnectionStrength : IValueObject
+    {
+
+        public int connectionStrength { get; private set; }
+
+        public ConnectionStrength(int strength)
+        {
+            if (strength >= 0 && strength <= 10)
+            {
+                this.connectionStrength = strength;
+            }
+            else
+            {
+                throw new BusinessRuleValidationException("The strength of the relationship should be between 0 and 10!");
+            }
+        }
+    }
+}
