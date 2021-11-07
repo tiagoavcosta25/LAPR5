61e77c7b16f06301cf5dcb57346ced7acedffcc2
1190682 <1190682@isep.ipp.pt>
2021-11-07 21:22:41 +0000

    LAPR5-26 #implementation #developing Profile Service Update Development

--- Code/Domain/Profiles/ProfileService.cs
+++ Code/Domain/Profiles/ProfileService.cs
@@ -21,7 +21,7 @@ namespace DDDSample1.Domain.Profiles
             var list = await this._repo.GetAllAsync();
             
             List<ProfileDto> listDto = list.ConvertAll<ProfileDto>(prof => 
-                new ProfileDto(prof.Id.AsGuid(),prof.Name.name, prof.Email.address, prof.PhoneNumber.phoneNumber, prof.DateOfBirth.date.Year, prof.DateOfBirth.date.Month, prof.DateOfBirth.date.Day));
+                new ProfileDto(prof.Id.AsGuid(),prof.Name.name, prof.Email.address, prof.PhoneNumber.phoneNumber, prof.DateOfBirth.date.Year, prof.DateOfBirth.date.Month, prof.DateOfBirth.date.Day, prof.EmotionalStatus.Status, prof.Facebook.Url, prof.LinkedIn.Url));
 
             return listDto;
         }
@@ -33,18 +33,18 @@ namespace DDDSample1.Domain.Profiles
             if(prof == null)
                 return null;
 
-            return new ProfileDto(prof.Id.AsGuid(),prof.Name.name, prof.Email.address, prof.PhoneNumber.phoneNumber, prof.DateOfBirth.date.Year, prof.DateOfBirth.date.Month, prof.DateOfBirth.date.Day);
+            return new ProfileDto(prof.Id.AsGuid(),prof.Name.name, prof.Email.address, prof.PhoneNumber.phoneNumber, prof.DateOfBirth.date.Year, prof.DateOfBirth.date.Month, prof.DateOfBirth.date.Day, prof.EmotionalStatus.Status, prof.Facebook.Url, prof.LinkedIn.Url);
         }
 
         public async Task<ProfileDto> AddAsync(CreatingProfileDto dto)
         {
-            var Profile = new Profile(dto.Name, dto.Email, dto.PhoneNumber, dto.DateOfBirth.Year, dto.DateOfBirth.Month, dto.DateOfBirth.Day);
+            var Profile = new Profile(dto.Name, dto.Email, dto.PhoneNumber, dto.DateOfBirth.Year, dto.DateOfBirth.Month, dto.DateOfBirth.Day, dto.EmotionalStatus, dto.Facebook, dto.LinkedIn);
 
             await this._repo.AddAsync(Profile);
 
             await this._unitOfWork.CommitAsync();
 
-            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day);
+            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day, Profile.EmotionalStatus.Status, Profile.Facebook.Url, Profile.LinkedIn.Url);
         }
 
         public async Task<ProfileDto> UpdateAsync(ProfileDto dto)
@@ -58,10 +58,13 @@ namespace DDDSample1.Domain.Profiles
             Profile.ChangeEmail(dto.Email);
             Profile.ChangePhoneNumber(dto.PhoneNumber);
             Profile.ChangeDateOfBirth(dto.DateOfBirth.Year, dto.DateOfBirth.Month, dto.DateOfBirth.Day);
+            Profile.ChangeEmotionalStatus(dto.EmotionalStatus);
+            Profile.ChangeFacebook(dto.Facebook);
+            Profile.ChangeLinkedIn(dto.LinkedIn);
             
             await this._unitOfWork.CommitAsync();
 
-            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day);
+            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day, Profile.EmotionalStatus.Status, Profile.Facebook.Url, Profile.LinkedIn.Url);
         }
 
         public async Task<ProfileDto> InactivateAsync(ProfileId id)
@@ -75,7 +78,7 @@ namespace DDDSample1.Domain.Profiles
             
             await this._unitOfWork.CommitAsync();
 
-            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day);
+            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day, Profile.EmotionalStatus.Status, Profile.Facebook.Url, Profile.LinkedIn.Url);
         }
 
         public async Task<ProfileDto> DeleteAsync(ProfileId id)
@@ -91,7 +94,7 @@ namespace DDDSample1.Domain.Profiles
             this._repo.Remove(Profile);
             await this._unitOfWork.CommitAsync();
 
-            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day);
+            return new ProfileDto(Profile.Id.AsGuid(),Profile.Name.name, Profile.Email.address, Profile.PhoneNumber.phoneNumber, Profile.DateOfBirth.date.Year, Profile.DateOfBirth.date.Month, Profile.DateOfBirth.date.Day, Profile.EmotionalStatus.Status, Profile.Facebook.Url, Profile.LinkedIn.Url);
         }
     }
 }
