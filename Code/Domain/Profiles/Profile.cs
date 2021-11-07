4fadcf0da7c15473004d906416ec3ea36da05fa3
1190682 <1190682@isep.ipp.pt>
2021-11-07 21:22:27 +0000

    LAPR5-26 #implementation #developing Profile Class Update Development

--- Code/Domain/Profiles/Profile.cs
+++ Code/Domain/Profiles/Profile.cs
@@ -6,10 +6,13 @@ namespace DDDSample1.Domain.Profiles
     public class Profile : Entity<ProfileId>, IAggregateRoot
     {
      
+        public ProfileName Name { get;  private set; }
+        public ProfileEmail Email { get;  private set; }
         public ProfileDateOfBirth DateOfBirth { get;  private set; }
         public ProfilePhoneNumber PhoneNumber { get;  private set; }
-        public ProfileEmail Email { get;  private set; }
-        public ProfileName Name { get;  private set; }
+        public ProfileEmotionalStatus EmotionalStatus { get;  private set; }
+        public ProfileFacebook Facebook { get;  private set; }
+        public ProfileLinkedIn LinkedIn { get;  private set; }
 
         public bool Active{ get;  private set; }
 
@@ -18,13 +21,16 @@ namespace DDDSample1.Domain.Profiles
             this.Active = true;
         }
 
-        public Profile(string name, string email, double phoneNumber, int year, int month, int day)
+        public Profile(string name, string email, double phoneNumber, int year, int month, int day, string emotionalStatus, string facebook, string linkedin)
         {
             this.Id = new ProfileId(Guid.NewGuid());
             this.Name = new ProfileName(name);
             this.Email = new ProfileEmail(email);
             this.PhoneNumber = new ProfilePhoneNumber(phoneNumber);
             this.DateOfBirth = new ProfileDateOfBirth(year, month, day);
+            this.EmotionalStatus = new ProfileEmotionalStatus(emotionalStatus);
+            this.Facebook = new ProfileFacebook(facebook);
+            this.LinkedIn = new ProfileLinkedIn(linkedin);
             this.Active = true;
         }
 
@@ -56,7 +62,27 @@ namespace DDDSample1.Domain.Profiles
                 throw new BusinessRuleValidationException("It is not possible to change the date of birth to an inactive Profile.");
             this.DateOfBirth = new ProfileDateOfBirth(year, month, day);
         }
-        public void MarkAsInative()
+
+        public void ChangeEmotionalStatus(string emotionalStatus)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the emotional status to an inactive Profile.");
+            this.EmotionalStatus = new ProfileEmotionalStatus(emotionalStatus);
+        }
+
+        public void ChangeFacebook(string facebook)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the facebook profile to an inactive Profile.");
+            this.Facebook = new ProfileFacebook(facebook);
+        }
+        
+        public void ChangeLinkedIn(string linkedIn)
+        {
+            if (!this.Active)
+                throw new BusinessRuleValidationException("It is not possible to change the linkedIn profile to an inactive Profile.");
+            this.LinkedIn = new ProfileLinkedIn(linkedIn);
+        }        public void MarkAsInative()
         {
             this.Active = false;
         }
