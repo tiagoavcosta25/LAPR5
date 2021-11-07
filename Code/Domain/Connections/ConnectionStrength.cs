60520de96cc128f9df3b4298e229506d28199ff1
1191460 <1191460@isep.ipp.pt>
2021-11-07 22:20:48 +0000

    LAPR5-32 #implementation #developing ConnectionStrength class updated

--- Code/Domain/Connections/ConnectionStrength.cs
+++ Code/Domain/Connections/ConnectionStrength.cs
@@ -1,9 +1,5 @@
 ﻿using DDDSample1.Domain.Shared;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DDDNetCore.Domain.Connections
 {
