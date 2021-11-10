0eb5ff3d05020b49a3e6dd9ca6913a32c4c00d9f
1191460 <1191460@isep.ipp.pt>
2021-11-10 14:58:35 +0000

    LAPR5-32 #implementation #developing Connection infrastructure fixed

---
+++ Code/Controllers/ConnectionsController.cs
@@ -0,0 +1,121 @@
+﻿using DDDNetCore.Domain.Connections;
+using DDDSample1.Domain.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DDDNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionsController : ControllerBase
+    {
+        private readonly ConnectionService _service;
+
+        public ConnectionsController(ConnectionService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/Connections
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ConnectionDto>>> GetAll()
+        {
+            return await _service.GetAllAsync();
+        }
+
+        // GET: api/Connections/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ConnectionDto>> GetGetById(Guid id)
+        {
+            var con = await _service.GetByIdAsync(new ConnectionId(id));
+
+            if (con == null)
+            {
+                return NotFound();
+            }
+
+            return con;
+        }
+
+        // POST: api/Connections
+        [HttpPost]
+        public async Task<ActionResult<ConnectionDto>> Create(CreatingConnectionDto dto)
+        {
+            try
+            {
+                var con = await _service.AddAsync(dto);
+
+                return CreatedAtAction(nameof(GetGetById), new { id = con.Id }, con);
+            }
+            catch (BusinessRuleValidationException ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+
+
+        // PUT: api/Connections/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ConnectionDto>> Update(Guid id, ConnectionDto dto)
+        {
+            if (id != dto.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                var con = await _service.UpdateAsync(dto);
+
+                if (con == null)
+                {
+                    return NotFound();
+                }
+                return Ok(con);
+            }
+            catch (BusinessRuleValidationException ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+
+        /* TODO: Checkar necessidade
+        // Inactivate: api/Connections/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ConnectionDto>> SoftDelete(Guid id)
+        {
+            var con = await _service.InactivateAsync(new ConnectionId(id));
+
+            if (con == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(con);
+        }
+        */
+
+        // DELETE: api/Connections/5
+        [HttpDelete("{id}/hard")]
+        public async Task<ActionResult<ConnectionDto>> HardDelete(Guid id)
+        {
+            try
+            {
+                var con = await _service.DeleteAsync(new ConnectionId(id));
+
+                if (con == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(con);
+            }
+            catch (BusinessRuleValidationException ex)
+            {
+                return BadRequest(new { Message = ex.Message });
+            }
+        }
+    }
+}
---
+++ Code/Infraestructure/Connections/ConnectionEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using DDDNetCore.Domain.Connections;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DDDNetCore.Infraestructure.Connections
+{
+    internal class ConnectionEntityTypeConfiguration : IEntityTypeConfiguration<Connection>
+    {
+        public void Configure(EntityTypeBuilder<Connection> builder)
+        {
+            //builder.ToTable("Connections", SchemaNames.DDDSample1);
+            builder.HasKey(b => b.Id);
+            builder.OwnsOne(x => x.ConnectionStrength);
+            builder.OwnsMany(x => x.Tags);
+        }
+    }
+}
