6b0c1ba6cb48a8c8398ef582e125d3233c0e95c1
beatrizseixas12 <1190424@isep.ipp.pt>
2021-11-11 13:04:27 +0000

    LAPR5-47 #n2 #developing N2, Process View Creation

---
+++ Code/Domain/Players/PlayerEmotionalStatus.cs
@@ -0,0 +1,31 @@
+using System;
+using DDDSample1.Domain.Shared;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DDDSample1.Domain.Players
+{
+    [ComplexType] 
+    public class PlayerEmotionalStatus : IValueObject
+    {
+
+        public string Status { get;  private set; }
+
+        public PlayerEmotionalStatus()
+        {
+        }
+
+        public PlayerEmotionalStatus(string status)
+        {
+            this.updateEmotionalStatus(status);
+            
+        }
+
+        public void updateEmotionalStatus(string status){
+            try{
+                this.Status = status; // verify
+            } catch{
+                throw new BusinessRuleValidationException("The provided emotional status is not valid.");
+            }
+        }
+    }
+}
