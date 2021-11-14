aea04499e401afa33e58a0164422abc2c8caa9d0
1190682 <1190682@isep.ipp.pt>
2021-11-14 02:15:54 +0000

    LAPR5-26 #implementation #fixing Get Player DTO Creation and Development

---
+++ Code/Domain/Players/GetPlayerDto.cs
@@ -0,0 +1,28 @@
+using DDDSample1.Domain.Players;
+using System;
+
+namespace DDDSample1.Domain.Players
+{
+    public class GetPlayerDto
+    {
+        public string Email { get;  set; }
+        public string Name { get;  set; }
+        public string PhoneNumber { get;  set; }
+        public DateTime DateOfBirth { get;  set; }
+        public string EmotionalStatus { get;  private set; }
+        public string Facebook { get;  private set; }
+        public string LinkedIn { get;  private set; }
+
+
+        public GetPlayerDto(string name, string email, string phoneNumber, int year, int month, int day, string emotionalStatus, string facebook, string linkedin)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.PhoneNumber = phoneNumber;
+            this.DateOfBirth = new DateTime(year, month, day);
+            this.EmotionalStatus = emotionalStatus;
+            this.Facebook = facebook;
+            this.LinkedIn = linkedin;
+        }
+    }
+}
