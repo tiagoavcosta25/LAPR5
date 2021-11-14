2151d32cf42ba575a3dfef42b741773d829b45b8
1190682 <1190682@isep.ipp.pt>
2021-11-14 03:41:25 +0000

    LAPR5-28 #review #fixing Update Player Dto Creation and Development

---
+++ Code/Domain/Players/UpdatePlayerDto.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System;
+
+namespace DDDSample1.Domain.Players
+{
+    public class UpdatePlayerDto
+    {
+        public Guid Id { get; set; }
+        public string Email { get;  set; }
+        public string Password { get;  set; }
+        public string Name { get;  set; }
+        public string PhoneNumber { get;  set; }
+        public DateTime DateOfBirth { get;  set; }
+        public string EmotionalStatus { get;  private set; }
+        public string Facebook { get;  private set; }
+        public string LinkedIn { get;  private set; }
+        public ICollection<string> Tags { get; set; }
+
+
+        public UpdatePlayerDto(Guid Id, string name, string email, string password, string phoneNumber, int year, int month, int day, string emotionalStatus, string facebook, 
+        string linkedin, ICollection<string> tags)
+        {
+            this.Id = Id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.PhoneNumber = phoneNumber;
+            this.DateOfBirth = new DateTime(year, month, day);
+            this.EmotionalStatus = emotionalStatus;
+            this.Facebook = facebook;
+            this.LinkedIn = linkedin;
+            Tags = tags;
+        }
+    }
+}
