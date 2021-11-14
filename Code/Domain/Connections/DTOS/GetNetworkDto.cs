35775a36682ca0029b8bc4143bb7809c404cb555
1190967 <1190967@isep.ipp.pt>
2021-11-14 05:06:36 +0000

    LAPR5-27 #review #developing Get Network DTO Creation and Development

---
+++ Code/Domain/Connections/DTOS/GetNetworkDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DDDNetCore.Domain.Connections.DTOS
+{
+    public class GetNetworkDto
+    {
+        public int Scope { get; set; }
+
+        public GetNetworkDto(int scope)
+        {
+            Scope = scope;
+        }
+    }
+}
