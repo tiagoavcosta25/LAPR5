981904d596c76f5416902a509326427ffd3762d3
1190682 <1190682@isep.ipp.pt>
2021-11-14 02:16:06 +0000

    LAPR5-26 #implementation #fixing Player Password Creation and Development

---
+++ Code/Domain/Players/PlayerPassword.cs
@@ -0,0 +1,32 @@
+using System;
+using DDDSample1.Domain.Shared;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DDDSample1.Domain.Players
+{
+    [ComplexType]
+    public class PlayerPassword : IValueObject
+    {
+    
+        public string password { get;  private set; }
+
+        public PlayerPassword()
+        {
+        }
+
+        public PlayerPassword(string pswd)
+        {
+            this.updatePassword(pswd);
+            
+        }
+
+        public void updatePassword(string pswd){
+            try{
+                
+                this.password = password;
+            } catch{
+                throw new BusinessRuleValidationException("The provided password is not valid.");
+            }
+        }
+    }
+}
