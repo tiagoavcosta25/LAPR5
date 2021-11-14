896677eb505b78c1c49cadab1915e2a972ee3a86
1191460 <1191460@isep.ipp.pt>
2021-11-14 21:54:06 +0000

    LAPR5-16 #unit #developing Unit Testing Development

--- Code/Tests/Controllers/ConnectionControllerTest.cs
+++ Code/Tests/Controllers/ConnectionControllerTest.cs
@@ -8,6 +8,7 @@ using Moq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DDDSample1.Domain.Shared;
+using System;
 
 namespace DDDNetCore.Tests.Controllers
 {
@@ -85,5 +86,178 @@ namespace DDDNetCore.Tests.Controllers
             
             Assert.Equal(lst, returnValue);
         }
+
+        //
+        [Fact]
+        public async Task GetAllConnections_ReturnsGettingConnectionDtoList()
+        {
+            // Arrange
+            GettingConnectionDto obj = new("john", "test@gmail.com", 5,
+                new List<string> { "tag"});
+            List<GettingConnectionDto> lst = new() { obj };
+            string playerEmail = "test@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.GetAllConnectionsAsync(playerEmail))
+                .ReturnsAsync(lst).Verifiable();
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.GetAllConnections(playerEmail);
+
+            // Assert
+            var returnValue = Assert.IsType<List<GettingConnectionDto>>(result.Value);
+            mockServ.Verify();
+            Assert.Equal(lst, returnValue);
+        }
+
+
+        [Fact]
+        public async Task GetAllConnections_WhenEmailNotValid()
+        {
+            // Arrange
+            GettingConnectionDto obj = new("john", "test@gmail.com", 5,
+                new List<string> { "tag" });
+            List<GettingConnectionDto> lst = new() { obj };
+            string playerEmail = "test@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.GetAllConnectionsAsync(playerEmail))
+                .ThrowsAsync(new BusinessRuleValidationException(""));
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.GetAllConnections(playerEmail);
+
+            // Assert
+            var actionResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.IsType<BadRequestObjectResult>(actionResult);
+        }
+
+
+
+
+        [Fact]
+        public async Task GetByEmails_ReturnsConnectionDto()
+        {
+            // Arrange
+            ConnectionDto obj = new(new Guid().ToString(), new Guid().ToString(), new Guid().ToString(),
+                3, new List<string> { "tag" });
+            string playerEmail = "test@gmail.com";
+            string friendEmail = "test2@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.GetByEmailsAsync(playerEmail, friendEmail))
+                .ReturnsAsync(obj).Verifiable();
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.GetByEmails(playerEmail, friendEmail);
+
+            // Assert
+            var returnValue = Assert.IsType<ConnectionDto>(result.Value);
+            mockServ.Verify();
+            Assert.Equal(obj, returnValue);
+        }
+
+        [Fact]
+        public async Task GetByEmails_WhenEmailNotValid()
+        {
+            // Arrange
+            ConnectionDto obj = new(new Guid().ToString(), new Guid().ToString(), new Guid().ToString(),
+                 3, new List<string> { "tag" });
+            string playerEmail = "test@gmail.com";
+            string friendEmail = "test2@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.GetByEmailsAsync(playerEmail, friendEmail))
+                .ThrowsAsync(new BusinessRuleValidationException(""));
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.GetByEmails(playerEmail, friendEmail);
+
+            // Assert
+            var actionResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.IsType<BadRequestObjectResult>(actionResult);
+        }
+
+
+
+
+        [Fact]
+        public async Task UpdateTagsAndStrength_ReturnsConnectionDto()
+        {
+            // Arrange
+            UpdatingConnectionDto param = new("test@gmail.com", "test2@gmail.com", 3, new List<string> { "tag"});
+            ConnectionDto obj = new(new Guid().ToString(), new Guid().ToString(), new Guid().ToString(),
+                3, new List<string> { "tag" });
+            string playerEmail = "test@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.UpdateTagsAndStrengthAsync(param))
+                .ReturnsAsync(obj).Verifiable();
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.UpdateTagsAndStrength(playerEmail, param);
+
+            // Assert
+            var actionResult = Assert.IsType<OkObjectResult>(result.Result);
+            
+            
+            var returnValue = Assert.IsType<ConnectionDto>(actionResult.Value);
+            mockServ.Verify();
+            
+            Assert.Equal(obj, returnValue);
+        }
+
+
+
+        [Fact]
+        public async Task UpdateTagsAndStrength_EmailNotEqual()
+        {
+            // Arrange
+            UpdatingConnectionDto param = new("error", "error", 3, new List<string> { "tag" });
+            ConnectionDto obj = new(new Guid().ToString(), new Guid().ToString(), new Guid().ToString(),
+                3, new List<string> { "tag" });
+            string playerEmail = "test@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.UpdateTagsAndStrengthAsync(param))
+                .ReturnsAsync(obj).Verifiable();
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.UpdateTagsAndStrength(playerEmail, param);
+
+            // Assert
+            var actionResult = Assert.IsType<BadRequestResult>(result.Result);
+            Assert.IsType<BadRequestResult>(actionResult);
+        }
+
+        [Fact]
+        public async Task UpdateTagsAndStrength_WhenEmailNotValid()
+        {
+            // Arrange
+            UpdatingConnectionDto param = new("test@gmail.com", "test2@gmail.com", 3, new List<string> { "tag" });
+            ConnectionDto obj = new(new Guid().ToString(), new Guid().ToString(), new Guid().ToString(),
+                3, new List<string> { "tag" });
+            string playerEmail = "test@gmail.com";
+
+            var mockServ = new Mock<IConnectionService>();
+            mockServ.Setup(s => s.UpdateTagsAndStrengthAsync(param))
+                .ThrowsAsync(new BusinessRuleValidationException(""));
+            var controller = new ConnectionsController(mockServ.Object);
+
+            // Act
+            var result = await controller.UpdateTagsAndStrength(playerEmail, param);
+
+            // Assert
+            var actionResult = Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.IsType<BadRequestObjectResult>(actionResult);
+        }
+
+
     }
 }
