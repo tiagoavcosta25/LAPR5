06f987d492c80583ebfcbdf32e71e58c2402d65b
1190682 <1190682@isep.ipp.pt>
2021-11-14 00:05:50 +0000

    LAPR5-44 #implementation #developing Approve Request DTO Creation and Development

---
+++ Code/Domain/ConnectionRequests/DTOS/ApproveRequestDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DDDNetCore.Domain.ConnectionRequests.DTOS
+{
+    public class ApproveRequestDto
+    {
+        public string Status { get; set; }
+        public string MiddleManToTargetMessage { get; set; }
+
+        public ApproveRequestDto(string status, string middleManToTargetMessage)
+        {
+            Status = status;
+            MiddleManToTargetMessage = middleManToTargetMessage;
+        }
+    }
+}
