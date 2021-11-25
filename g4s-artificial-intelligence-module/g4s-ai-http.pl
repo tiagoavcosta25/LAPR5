77f9dd3633192cd3e1e10f231453c3b1ce023d4e
1190967 <1190967@isep.ipp.pt>
2021-11-25 17:46:15 +0000

    LAPR5-85 #ai #implementation HTTP PROLOG Safest_Route Method Changes

--- g4s-artificial-intelligence-module/g4s-ai-http.pl
+++ g4s-artificial-intelligence-module/g4s-ai-http.pl
@@ -59,18 +59,19 @@ dfs_safest_route(Orig,Dest,Threshold, Strength, Path):-dfs2_safest_route(Orig,De
 
 dfs2_safest_route(Dest,Dest,LA, _, S,S, Cam):-!,reverse(LA,Cam).
 dfs2_safest_route(Act,Dest,LA,Threshold,Strength, ReturnStrength,Cam):-node(NAct,Act,_),(connection(NAct,NX,N_StrengthA,N_StrengthB);connection(NX,NAct,N_StrengthA,N_StrengthB)),
-    node(NX,X,_),\+ member(X,LA),N_StrengthA > Threshold,
-    N_StrengthB > Threshold, FinalStrength1 is N_StrengthA + N_StrengthB,FinalStrength is Strength + FinalStrength1, dfs2_safest_route(X,Dest,[X|LA], Threshold, FinalStrength, ReturnStrength,Cam).
+    node(NX,X,_),\+ member(X,LA),N_StrengthA >= Threshold,
+    N_StrengthB >= Threshold, FinalStrength1 is N_StrengthA + N_StrengthB,FinalStrength is Strength + FinalStrength1, dfs2_safest_route(X,Dest,[X|LA], Threshold, FinalStrength, ReturnStrength,Cam).
 
 
 safest_route(Orig,Dest,Threshold, SafestPath):-
 		get_time(Initial_Time),
 		(find_safest_route(Orig,Dest, Threshold);true),
 		retract(safest_current_route(SafestPath,Strength)),
-		get_time(End_Time),
+		((Strength >= 0, !, get_time(End_Time),
 		T is End_Time - Initial_Time,
 		write('Time:'),write(T),nl,
-		write('Strength: '), write(Strength),nl.
+		write('Strength: '), write(Strength),nl);
+		write('No path found.'),false).
 
 find_safest_route(Orig,Dest,Threshold):-
 		asserta(safest_current_route(_,-10000)),
