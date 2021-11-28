bc47b897ea31927644b61fdf2d11a1c92a6c088d
1190967 <1190967@isep.ipp.pt>
2021-11-28 22:45:08 +0000

    LAPR5-85 #ai #implementation HTTP PROLOG Safest Route, Find, Update and DFS Method Adjustments

--- g4s-artificial-intelligence-module/g4s-ai-http.pl
+++ g4s-artificial-intelligence-module/g4s-ai-http.pl
@@ -97,36 +97,43 @@ intersect([_|L1],L2, LI):- intersect(L1,L2,LI).
 
 % safest route
 
-:-dynamic safest_current_route/2.
+:-dynamic safest_currentRoute/2.
 
-dfs_safest_route(Orig,Dest,Threshold, Strength, Path):-dfs2_safest_route(Orig,Dest,[Orig],Threshold, 0, Strength, Path).
+safest_dfs(Orig, Dest, Threshold, Strength, Path):- safest_dfsAux(Orig, Dest, [Orig], Threshold, 0, Strength, Path).
 
-dfs2_safest_route(Dest,Dest,LA, _, S,S, Cam):-!,reverse(LA,Cam).
-dfs2_safest_route(Act,Dest,LA,Threshold,Strength, ReturnStrength,Cam):-node(NAct,Act,_),(connection(NAct,NX,N_StrengthA,N_StrengthB);connection(NX,NAct,N_StrengthA,N_StrengthB)),
-    node(NX,X,_),\+ member(X,LA),N_StrengthA >= Threshold,
-    N_StrengthB >= Threshold, FinalStrength1 is N_StrengthA + N_StrengthB,FinalStrength is Strength + FinalStrength1, dfs2_safest_route(X,Dest,[X|LA], Threshold, FinalStrength, ReturnStrength,Cam).
+safest_dfsAux(Dest, Dest, AuxList, _ , Strength, Strength, Path):-!, reverse(AuxList,Path).
+safest_dfsAux(Current, Dest, AuxList, Threshold, Strength, ReturnStrength, Path):-
+		node(CurrentID,Current,_),
+		(connection(CurrentID, FriendID, StrengthA, StrengthB);
+		connection(FriendID, CurrentID, StrengthA, StrengthB)),
+		node(FriendID, Friend, _),
+		\+ member(Friend, AuxList),
+		StrengthA >= Threshold,
+		StrengthB >= Threshold,
+		CurrentStrength is Strength + StrengthA + StrengthB,
+		safest_dfsAux(Friend, Dest, [Friend | AuxList], Threshold, CurrentStrength, ReturnStrength, Path).
 
 
-safest_route(Orig,Dest,Threshold, SafestPath):-
+safest_route(Orig, Dest, Threshold, SafestPath):-
 		get_time(Initial_Time),
-		(find_safest_route(Orig,Dest, Threshold);true),
-		retract(safest_current_route(SafestPath,Strength)),
+		(safest_findRoute(Orig,Dest, Threshold);true),
+		retract(safest_currentRoute(SafestPath,Strength)),
 		((Strength >= 0, !, get_time(End_Time),
 		T is End_Time - Initial_Time,
 		write('Time:'),write(T),nl,
 		write('Strength: '), write(Strength),nl);
 		write('No path found.'),false).
 
-find_safest_route(Orig,Dest,Threshold):-
-		asserta(safest_current_route(_,-10000)),
-		dfs_safest_route(Orig,Dest,Threshold, Strength, PathList),
-		update_safest_route(Strength, PathList),
+safest_findRoute(Orig, Dest, Threshold):-
+		asserta(safest_currentRoute(_,-10000)),
+		safest_dfs(Orig, Dest, Threshold, Strength, PathList),
+		safest_updateRoute(Strength, PathList),
 		fail.
 
-update_safest_route(Strength, PathList):-
-		safest_current_route(_,Current_Strength),
-		Strength > Current_Strength,retract(safest_current_route(_,_)),
-		asserta(safest_current_route(PathList,Strength)).
+safest_updateRoute(Strength, PathList):-
+		safest_currentRoute(_,Current_Strength),
+		Strength > Current_Strength, retract(safest_currentRoute(_,_)),
+		asserta(safest_currentRoute(PathList,Strength)).
 
 
 % player suggestion
