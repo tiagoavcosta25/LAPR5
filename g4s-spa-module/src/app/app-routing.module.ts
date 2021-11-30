92900bc3d990975d8ec36467aaf3af6ae6af79b8
Pedro Santos <1190967@isep.ipp.pt>
2021-11-30 23:21:19 +0000

    LAPR5-118 #angular #implementation Angular App Module and Routing Changes

--- g4s-spa-module/src/app/app-routing.module.ts
+++ g4s-spa-module/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { RegisterPlayerComponent } from './player/register-player/register-player.component';
 import { LoginComponent } from './layout/login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'dashboard',  component: DashboardComponent },
+  { path: 'register-player',  component: RegisterPlayerComponent },
   { path: 'login',  component: LoginComponent }
 ];
 
--- g4s-spa-module/src/app/app.module.ts
+++ g4s-spa-module/src/app/app.module.ts
@@ -3,20 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SidePanelComponent } from './layout/side-panel/side-panel.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
 import { LoginComponent } from './layout/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { RegisterPlayerComponent } from './player/register-player/register-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidePanelComponent,
+    DashboardComponent,
     LoginComponent,
-    LayoutComponent
+    LayoutComponent,
+    RegisterPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
