483ceb5aed8a599e2025b3a367a0682751d1b143
Pedro Santos <1190967@isep.ipp.pt>
2021-12-02 19:25:12 +0000

    LAPR5-118 #angular #implementation Angular Player Model Creation

---
+++ g4s-spa-module/src/app/shared/models/player.model.ts
@@ -0,0 +1,11 @@
+export class Player {
+    id?: number;
+    name?: string;
+    email?: string;
+    dateOfBirth?: string;
+    phoneNumber?: number;
+    emotionalStatus?: string;
+    facebook?: string;
+    linkedIn?: string;
+    tags?: string[];
+}
