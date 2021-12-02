13d6880bec095b5107ef08cd42edd46e83d41cf4
Pedro Santos <1190967@isep.ipp.pt>
2021-12-02 14:41:42 +0000

    LAPR5-85 #ai #implementation HTTP PROLOG Cloud HTTP Config

---
+++ g4s-artificial-intelligence-module/g4s-ai-http-config-cloud.pl
@@ -0,0 +1,6 @@
+server_port(4999). % Server port
+module_socialnetwork_host('localhost'). % Host who runs the SocialNetwork module
+module_socialnetwork_port(5000).
+api_url("https://socialnetworkapi51.azurewebsites.net/api/").
+players_url("https://socialnetworkapi51.azurewebsites.net/api/players/").
+connections_url("https://socialnetworkapi51.azurewebsites.net/api/connections/").
