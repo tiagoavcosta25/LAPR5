d93a156bc6b2d2ae7de1cbb961907b2b1b43dda0
Pedro Santos <1190967@isep.ipp.pt>
2021-12-02 19:27:40 +0000

    LAPR5-118 #angular #implementation Angular Routing and Module Changes

--- g4s-spa-module/src/app/app-routing.module.ts
+++ g4s-spa-module/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
-import { RegisterPlayerComponent } from './player/register-player/register-player.component';
+import { DashboardComponent } from './modules/layout/components/dashboard/dashboard.component';
+import { RegisterPlayerComponent } from './modules/player/components/register-player/register-player.component';
 import { LoginComponent } from './layout/login/login.component';
 import { HeaderComponent } from './layout/header/header.component';
 
--- g4s-spa-module/src/app/app.module.ts
+++ g4s-spa-module/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { DashboardComponent } from './modules/layout/components/dashboard/dashboard.component';
 import { LoginComponent } from './layout/login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,8 +13,9 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
-import { RegisterPlayerComponent } from './player/register-player/register-player.component';
+import { RegisterPlayerComponent } from './modules/player/components/register-player/register-player.component';
 import { HeaderComponent } from './layout/header/header.component';
+import { PlayerComponent } from './modules/player/player.component';
 
 
 @NgModule({
@@ -24,7 +25,8 @@ import { HeaderComponent } from './layout/header/header.component';
     LoginComponent,
     LayoutComponent,
     RegisterPlayerComponent,
-    HeaderComponent
+    HeaderComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,
