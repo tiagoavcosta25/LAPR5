50e046f79ad604345b372ff4d22fc5ab64c5fd9c
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 16:03:10 +0000

    LAPR5-118 #angular #implementation AngularPlayer Service Changes

--- g4s-spa-module/src/app/modules/player/services/player.service.ts
+++ g4s-spa-module/src/app/modules/player/services/player.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 import { Player } from 'src/shared/models/player/player.model';
-
-const httpOptions = {
-  headers: new HttpHeaders({
-    'Content-Type':  'application/json'
-  })
-};
+import { CreatingPlayer } from '../models/creating-player.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  playersUrl = 'https://socialnetworkapi51.azurewebsites.net/api/players';  // URL to web api
+  playerUrl: string = "https://socialnetworkapi51.azurewebsites.net/api/players";  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json'
+    })
+  };
 
   constructor(
     private http: HttpClient) {
@@ -24,28 +25,41 @@ export class PlayerService {
 
   /** GET players from the server */
   getPlayers(): Observable<Player[]> {
-    return this.http.get<Player[]>(this.playersUrl);
-  }
-
-  /* GET players by email */
-  getPlayerByEmail(email: string): Observable<Player[]> {
-    email = email.trim();
-
-    // Add safe, URL encoded search parameter if there is a search term
-    const options = email ?
-     { params: new HttpParams().set('email', email) } : {};
-
-    return this.http.get<Player[]>(this.playersUrl, options);
+    return this.http.get<Player[]>(this.playerUrl).pipe(
+      catchError(this.handleError)
+    );
   }
 
   /** POST: add a new player to the database */
-  registerPlayer(player: Player): Observable<Player> {
-    return this.http.post<Player>(this.playersUrl, player, httpOptions);
+  registerPlayer(player: CreatingPlayer): Observable<Player> {
+    return this.http.post<Player>(this.playerUrl, player, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
   }
 
   /** DELETE: delete the player from the server */
   deletePlayer(id: number): Observable<unknown> {
-    const url = `${this.playersUrl}/${id}`; // DELETE api/players/42
-    return this.http.delete(url, httpOptions);
+    const url = `${this.playerUrl}/${id}`; // DELETE api/players/42
+    return this.http.delete(url, this.httpOptions);
+  }
+
+  /** GET: retorna player do servidor */
+  getPlayerById(id: string): Observable<Player> {
+    return this.http.get<Player>(this.playerUrl + id).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.error('An error occurred: ', err.error.errors.message);
+    if (err.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', err.error.message);
+    }
+    else {
+      console.error(
+        `Web Api returned code ${err.status}, ` + ` Response body was: ${err.error}`
+      );
+    }
+    return throwError(() => err);
   }
 }
