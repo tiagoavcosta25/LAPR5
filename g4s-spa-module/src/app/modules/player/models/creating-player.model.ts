bc04b257e12db202738b2c14bbe514d2444ebc0d
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 16:01:22 +0000

    LAPR5-118 #angular #implementation Angular CreatingPlayerDTO Model Creation

---
+++ g4s-spa-module/src/app/modules/player/models/creating-player.model.ts
@@ -0,0 +1,14 @@
+export class CreatingPlayer {
+    name: string;
+    email: string;
+    password: string;
+    year: number;
+    month: number;
+    day: number;
+    phoneNumber: number;
+    emotionalStatus: string;
+    facebook: string;
+    linkedIn: string;
+    tags: string[] = [];
+}
+
