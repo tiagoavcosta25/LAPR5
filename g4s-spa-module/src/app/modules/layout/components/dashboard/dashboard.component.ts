342514bae9a96ef73da9f4d0bcc56d2e31fbfd02
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 21:11:32 +0000

    LAPR5-118 #angular #implementation Angular Dashboard Changes

--- g4s-spa-module/src/app/modules/layout/components/dashboard/dashboard.component.ts
+++ g4s-spa-module/src/app/modules/layout/components/dashboard/dashboard.component.ts
@@ -10,31 +10,43 @@ export class DashboardComponent implements OnInit {
   showGetPlayers: boolean = true;
   showRegisterPlayer: boolean = false;
   showLogin: boolean = false;
+  showRequestIntroduction: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
     this.showGetPlayers = true;
+    this.showRequestIntroduction = false;
     this.showRegisterPlayer = false;
     this.showLogin = false;
   }
 
   openGetPlayers(){
     this.showGetPlayers = true;
+    this.showRequestIntroduction = false;
     this.showLogin = false;
     this.showRegisterPlayer = false;
   }
 
   openRegisterPlayer(){
     this.showGetPlayers = false;
+    this.showRequestIntroduction = false;
     this.showLogin = false;
     this.showRegisterPlayer = true;
   }
 
   openLogin(){
     this.showGetPlayers = false;
+    this.showRequestIntroduction = false;
     this.showRegisterPlayer = false;
     this.showLogin = true;
+  }
+
+  openRequestIntroduction(){
+    this.showGetPlayers = false;
+    this.showRequestIntroduction = true;
+    this.showRegisterPlayer = false;
+    this.showLogin = false;
   }
 
 }
