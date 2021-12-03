c34fc05220159f497c7ec106cae6580d3f8edcfe
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 02:29:13 +0000

    LAPR5-118 #angular #implementation Angular Shared Module Creation and Development

---
+++ g4s-spa-module/src/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedRoutingModule } from './shared-routing.module';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    SharedRoutingModule
+  ],
+
+  providers: []
+})
+export class SharedModule { }
