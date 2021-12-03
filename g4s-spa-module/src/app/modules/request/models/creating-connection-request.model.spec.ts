c983ea8b580e6c5f1a12bf4f082de1d8e80b766b
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 21:16:15 +0000

    LAPR5-104 #angular #implementation Angular Creating Connection Request and Get Mutual Friends DTO Creation and Development

---
+++ g4s-spa-module/src/app/modules/request/models/creating-connection-request.model.spec.ts
@@ -0,0 +1,7 @@
+import { CreatingConnectionRequest } from './creating-connection-request.model';
+
+describe('CreatingConnectionRequest', () => {
+  it('should create an instance', () => {
+    expect(new CreatingConnectionRequest()).toBeTruthy();
+  });
+});
---
+++ g4s-spa-module/src/app/modules/request/models/creating-connection-request.model.ts
@@ -0,0 +1,11 @@
+export class CreatingConnectionRequest {
+    player: string;
+    target: string;
+    playerToTargetMessage: string;
+    currentStatus: string;
+    strength: number;    
+    tags: string[];
+    middleMan: string;
+    playerToMiddleManMessage: string;
+    middleManToTargetMessage: string;
+}
