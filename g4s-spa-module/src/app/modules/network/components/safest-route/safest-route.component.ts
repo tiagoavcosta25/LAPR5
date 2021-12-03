338ec93752a780aba2eac32b5d51954a83652292
Pedro Santos <1190967@isep.ipp.pt>
2021-12-03 02:29:47 +0000

    LAPR5-118 #angular #implementation Angular Safest Route Component Creation

---
+++ g4s-spa-module/src/app/modules/network/components/safest-route/safest-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-safest-route',
+  templateUrl: './safest-route.component.html',
+  styleUrls: ['./safest-route.component.css']
+})
+export class SafestRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
