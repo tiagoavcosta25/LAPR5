e0a597318efb10fe964577ac606ab8a165804d2d
1191460 <1191460@isep.ipp.pt>
2021-12-03 04:19:56 +0000

    LAPR5-129 #angular #implementation Angular Login changes

--- g4s-spa-module/src/app/app-routing.module.ts
+++ g4s-spa-module/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './modules/layout/components/dashboard/dashboard.component';
 import { RegisterPlayerComponent } from './modules/player/components/register-player/register-player.component';
 import { GetPlayersComponent } from './modules/player/components/get-players/get-players.component';
-import { LoginComponent } from './layout/login/login.component';
+import { LoginComponent } from './modules/login/login.component';
 import { HeaderComponent } from './modules/layout/components/header/header.component';
 
 const routes: Routes = [
--- g4s-spa-module/src/app/app.module.ts
+++ g4s-spa-module/src/app/app.module.ts
@@ -4,8 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './modules/layout/components/dashboard/dashboard.component';
-import { LoginComponent } from './layout/login/login.component';
-import { LayoutComponent } from './layout/layout.component';
+import { LoginComponent } from './modules/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -31,7 +30,6 @@ import { CreateIntRequestComponent } from './modules/request/components/create-i
     AppComponent,
     DashboardComponent,
     LoginComponent,
-    LayoutComponent,
     RegisterPlayerComponent,
     HeaderComponent,
     PlayerComponent,
