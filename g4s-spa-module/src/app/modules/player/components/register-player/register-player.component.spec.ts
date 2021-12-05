39693295f169e3c72e420a4427743d19e70069a3
Pedro Santos <1190967@isep.ipp.pt>
2021-12-05 23:13:51 +0000

    LAPR5-104 #angular #implementation Angular Register Player Testing

--- g4s-spa-module/src/app/modules/player/components/register-player/register-player.component.spec.ts
+++ g4s-spa-module/src/app/modules/player/components/register-player/register-player.component.spec.ts
@@ -1,14 +1,33 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PlayerService } from '../../services/player.service';
+import { of } from 'rxjs';
 
 import { RegisterPlayerComponent } from './register-player.component';
+import { Player } from 'src/shared/models/player/player.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CreatingPlayer } from '../../models/creating-player.model';
+import { By } from '@angular/platform-browser';
 
 describe('RegisterPlayerComponent', () => {
   let component: RegisterPlayerComponent;
   let fixture: ComponentFixture<RegisterPlayerComponent>;
 
+  let pService: PlayerService;
+
+  let spy: jasmine.Spy;
+  let de: DebugElement;
+
+  let p = new Player();
+  let p2 = new CreatingPlayer();
+  let np = new CreatingPlayer();
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterPlayerComponent ]
+      declarations: [RegisterPlayerComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      providers: [PlayerService]
     })
     .compileComponents();
   });
@@ -16,10 +35,44 @@ describe('RegisterPlayerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterPlayerComponent);
     component = fixture.componentInstance;
+
+    fixture = TestBed.createComponent(RegisterPlayerComponent);
+    component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    pService = de.injector.get<PlayerService>(PlayerService);
+
+    p.id = "1", p.name = "user1", p.email = "email1@gmail.com", p.year = 1,
+    p.month = 1, p.day = 1, p.phoneNumber = 1, p.emotionalStatus = "joyful", p.facebook = "facebook.com",
+    p.linkedIn = "linedIn.com", p.tags = [];
+
+    p2.name = "user1", p2.email = "email1@gmail.com", p2.year = 1,
+    p2.month = 1, p2.day = 1, p2.phoneNumber = 1, p2.emotionalStatus = "joyful", p2.facebook = "facebook.com",
+    p2.linkedIn = "linedIn.com", p2.tags = [];
+    spy = spyOn(pService, 'registerPlayer').and.returnValue(of(p));
+    
     fixture.detectChanges();
   });
 
+  
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('p should be np', () =>{
+    expect(component.p.name).toBe(np.name);
+  });
+
+  it('success should be undefined', () =>{
+    expect(component.success).toBeUndefined();
+  });
+
+  it('successMessage should be Player created sucessfully!', () =>{
+    expect(component.successMessage).toBe("Player created sucessfully!")
+  });
+
+  it('successMessage should be There was an error creating a player!', () =>{
+    expect(component.errorMessage).toBe("There was an error creating a player!")
   });
 });
--- g4s-spa-module/src/app/modules/player/components/update-player/update-player.component.spec.ts
+++ g4s-spa-module/src/app/modules/player/components/update-player/update-player.component.spec.ts
@@ -1,4 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { DobPlayer } from '../../models/dob-player.model copy';
+import { PlayerService } from '../../services/player.service';
 
 import { UpdatePlayerComponent } from './update-player.component';
 
@@ -6,20 +12,63 @@ describe('UpdatePlayerComponent', () => {
   let component: UpdatePlayerComponent;
   let fixture: ComponentFixture<UpdatePlayerComponent>;
 
+  let pService: PlayerService;
+
+  let spy: jasmine.Spy;
+  let spy2: jasmine.Spy;
+  let de: DebugElement;
+
+  let p = new DobPlayer();
+  let np = new DobPlayer();
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpdatePlayerComponent ]
+      declarations: [ UpdatePlayerComponent ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      providers: [PlayerService]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    localStorage.setItem('currentPlayer', 'email1@gmail.com');
     fixture = TestBed.createComponent(UpdatePlayerComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    pService = de.injector.get<PlayerService>(PlayerService);
+
+    p.id = "1", p.name = "user1", p.email = "email1@gmail.com", p.dateOfBirth = "2000-1-1T00:00:00",
+     p.phoneNumber = 1, p.emotionalStatus = "joyful", p.facebook = "facebook.com",
+    p.linkedIn = "linedIn.com", p.tags = [];
+
+    spy = spyOn(pService, 'getOnlyPlayerByEmail').and.returnValue(of(p));
+    spy2 = spyOn(pService, 'updatePlayer').and.returnValue(of(p));
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('p should be p', () =>{
+    expect(component.p).toBe(p);
+  });
+
+  it('success should be undefined', () =>{
+    expect(component.success).toBeUndefined();
+  });
+
+  it('successMessage should be Player updated sucessfully!', () =>{
+    expect(component.successMessage).toBe("Player updated sucessfully!")
+  });
+
+  it('successMessage should be There was an error updating a player!', () =>{
+    expect(component.errorMessage).toBe("There was an error updating a player!")
+  });
+
+  it('currentPlayer should be email1@gmail.com', () =>{
+    expect(localStorage.getItem('currentPlayer')).toBe("email1@gmail.com")
   });
 });
