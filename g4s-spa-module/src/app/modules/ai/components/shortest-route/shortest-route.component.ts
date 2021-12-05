4fa8f64258f9baa59fce6b6f87773dd77cdf4f34
1190682 <1190682@isep.ipp.pt>
2021-12-05 02:03:37 +0000

    LAPR5-96 #angular #implementation Angular Shortest Route Component Development

---
+++ g4s-spa-module/src/app/modules/ai/components/shortest-route/shortest-route.component.ts
@@ -0,0 +1,88 @@
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { PlayerService } from 'src/app/modules/player/services/player.service';
+import { Player } from 'src/shared/models/player/player.model';
+import { AiService } from '../../services/ai.service';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-shortest-route',
+  templateUrl: './shortest-route.component.html',
+  styleUrls: ['./shortest-route.component.css']
+})
+export class ShortestRouteComponent implements OnInit {
+
+  step: number;
+
+  shortestRoute: string[];
+
+  error: boolean;
+
+  players: Player[];
+
+  tempPlayers: Player[];
+
+
+  constructor(private aiService: AiService,
+    private pService: PlayerService,
+    private location: Location,
+    private spinner: NgxSpinnerService) { }
+
+  ngOnInit(): void {
+    this.getshortestRoute("email1@gmail.com", "email3@gmail.com");
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  getshortestRoute(emailPlayer: string, emailTarget: string): void {
+    this.spinner.show();
+    this.aiService.getshortestRoute(emailPlayer, emailTarget).subscribe({ next: data => {
+      this.shortestRoute = data;
+      this.getPlayers();
+      this.spinner.hide();
+    },
+      error: _error => {
+        this.spinner.hide();
+        this.error = true;
+      }
+    });
+  } 
+
+  getPlayers(): void {
+    this.pService.getPlayers().subscribe({ next: data => {
+      this.tempPlayers = data;
+      var lst = [];
+      for(let playerId of this.shortestRoute) {
+        for(let player of this.tempPlayers) {
+          if(player.id == playerId) {
+            lst.push(player);
+            break;
+          }
+        }
+      }
+      this.players = lst;
+      this.spinner.hide();
+    },
+      error: _error => {
+        this.spinner.hide();
+        this.error = true;
+      }
+    });
+  } 
+
+  getLastPlayerName(): string {
+    return this.players[this.players.length - 1].name;
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  refresh(): void {
+    window.location.reload();
+  }
+
+  }
