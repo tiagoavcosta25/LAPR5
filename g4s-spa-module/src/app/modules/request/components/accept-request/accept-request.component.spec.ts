6040376e66ca1b6ef534dc7a60a21fff0541cfa0
Pedro Santos <1190967@isep.ipp.pt>
2021-12-05 23:13:40 +0000

    LAPR5-104 #angular #implementation Angular Request Introduction Testing

--- g4s-spa-module/src/app/modules/request/components/accept-request/accept-request.component.spec.ts
+++ g4s-spa-module/src/app/modules/request/components/accept-request/accept-request.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { ConnectionService } from 'src/app/modules/connection/services/connection.service';
+import { PlayerService } from 'src/app/modules/player/services/player.service';
+import { RequestService } from '../../services/request.service';
 
 import { AcceptRequestComponent } from './accept-request.component';
 
@@ -6,20 +13,63 @@ describe('AcceptRequestComponent', () => {
   let component: AcceptRequestComponent;
   let fixture: ComponentFixture<AcceptRequestComponent>;
 
+  let cService: ConnectionService;
+  let rService: RequestService;
+  let pService: PlayerService;
+
+  let spy: jasmine.Spy;
+  let spy2: jasmine.Spy;
+  let spy3: jasmine.Spy;
+  let spy4: jasmine.Spy;
+  let de: DebugElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AcceptRequestComponent ]
+      declarations: [ AcceptRequestComponent ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      providers: [PlayerService, RequestService, ConnectionService]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    localStorage.setItem('currentPlayer', 'email1@gmail.com');
     fixture = TestBed.createComponent(AcceptRequestComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    pService = de.injector.get<PlayerService>(PlayerService);
+    rService = de.injector.get<RequestService>(RequestService);
+    cService = de.injector.get<ConnectionService>(ConnectionService);
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('success should be undefined', () =>{
+    expect(component.success).toBeUndefined();
+  });
+
+  it('error should be undefined', () =>{
+    expect(component.error).toBeUndefined();
+  });
+
+  it('successMessage should be undefined', () =>{
+    expect(component.successMessage).toBeUndefined();
+  });
+
+  it('successMessage should be Request accepted sucessfully! Refreshing in 2 seconds.', () =>{
+    expect(component.successMessageAccept).toBe("Request accepted sucessfully! Refreshing in 2 seconds.")
+  });
+
+  it('successMessage should be Request accepted sucessfully! Refreshing in 2 seconds.', () =>{
+    expect(component.successMessageDeny).toBe("Request accepted sucessfully! Refreshing in 2 seconds.")
+  });
+
+  it('currentPlayer should be email1@gmail.com', () =>{
+    expect(localStorage.getItem('currentPlayer')).toBe("email1@gmail.com")
   });
 });
--- g4s-spa-module/src/app/modules/request/components/request-introduction/request-introduction.component.spec.ts
+++ g4s-spa-module/src/app/modules/request/components/request-introduction/request-introduction.component.spec.ts
@@ -1,4 +1,16 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { ConnectionService } from 'src/app/modules/connection/services/connection.service';
+import { DobPlayer } from 'src/app/modules/player/models/dob-player.model copy';
+import { PlayerService } from 'src/app/modules/player/services/player.service';
+import { Player } from 'src/shared/models/player/player.model';
+import { ConnectionRequest } from 'src/shared/models/requests/connection-request.model';
+import { IntroductionRequest } from 'src/shared/models/requests/introduction-request.model';
+import { CreatingConnectionRequest } from '../../models/creating-connection-request.model';
+import { RequestService } from '../../services/request.service';
 
 import { RequestIntroductionComponent } from './request-introduction.component';
 
@@ -6,20 +18,85 @@ describe('CreateIntRequestComponent', () => {
   let component: RequestIntroductionComponent;
   let fixture: ComponentFixture<RequestIntroductionComponent>;
 
+  let cService: ConnectionService;
+  let rService: RequestService;
+  let pService: PlayerService;
+
+  let spy: jasmine.Spy;
+  let spy2: jasmine.Spy;
+  let spy3: jasmine.Spy;
+  let spy4: jasmine.Spy;
+  let de: DebugElement;
+
+  let c = new CreatingConnectionRequest();
+  let c2 = new IntroductionRequest();
+  let mutualFriends : Player[];
+  mutualFriends = [];
+  let reachablePlayer = new Player;
+  let player = new DobPlayer;
+  let mutualFriend = new Player;
+  let reachablePlayers: Player[];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RequestIntroductionComponent ]
+      declarations: [ RequestIntroductionComponent ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule],
+      providers: [PlayerService, RequestService, ConnectionService]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    localStorage.setItem('currentPlayer', 'email1@gmail.com');
     fixture = TestBed.createComponent(RequestIntroductionComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+
+    pService = de.injector.get<PlayerService>(PlayerService);
+    rService = de.injector.get<RequestService>(RequestService);
+    cService = de.injector.get<ConnectionService>(ConnectionService);
+
+    spy = spyOn(cService, 'getMutualFriends').and.returnValue(of(mutualFriends));
+    spy2 = spyOn(cService, 'getReachablePlayers').and.returnValue(of(reachablePlayers));
+    spy2 = spyOn(pService, 'getOnlyPlayerByEmail').and.returnValue(of(player));
+    spy2 = spyOn(rService, 'registerIntroductionRequest').and.returnValue(of(c2));
+    
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('mutualFriend should be mt', () =>{
+    expect(component.mutualFriend.name).toBe(mutualFriend.name);
+  });
+
+  it('mutualFriends should be mts', () =>{
+    expect(component.mutualFriends.length).toBe(mutualFriends.length);
+  });
+
+  it('reachablePlayer should be rp', () =>{
+    expect(component.reachablePlayer.name).toBe(reachablePlayer.name);
+  });
+
+  it('player should be p', () =>{
+    expect(component.player.name).toBe(player.name);
+  });
+
+  it('success should be undefined', () =>{
+    expect(component.success).toBeUndefined();
+  });
+
+  it('successMessage should be Introduction requested with sucess', () =>{
+    expect(component.successMessage).toBe("Introduction requested with sucess")
+  });
+
+  it('successMessage should be There was an error with the request!', () =>{
+    expect(component.errorMessage).toBe("There was an error with the request!")
+  });
+
+  it('currentPlayer should be email1@gmail.com', () =>{
+    expect(localStorage.getItem('currentPlayer')).toBe("email1@gmail.com")
   });
 });
