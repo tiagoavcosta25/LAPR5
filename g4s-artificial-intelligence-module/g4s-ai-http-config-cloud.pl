079d11e20e431c06274731d2426cd5b01a50c6ec
1191460 <1191460@isep.ipp.pt>
2021-12-08 09:32:12 +0000

    LAPR5-150 #ai #implementation HTTPS on cloud development

--- g4s-artificial-intelligence-module/g4s-ai-http-config-cloud.pl
+++ g4s-artificial-intelligence-module/g4s-ai-http-config-cloud.pl
@@ -1,4 +1,4 @@
-server_port(4999). % Server port
+server_port(443). % Server port
 module_socialnetwork_host('localhost'). % Host who runs the SocialNetwork module
 module_socialnetwork_port(5000).
 api_url("https://socialnetworkapi51.azurewebsites.net/api/").
--- g4s-spa-module/src/environments/environment.prod.ts
+++ g4s-spa-module/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://socialnetworkapi51.azurewebsites.net/api',
-  aiUrl: 'https://socialnetworkaig51.westeurope.cloudapp.azure.com:4999'
+  aiUrl: 'https://socialnetworkaig51.westeurope.cloudapp.azure.com'
 };
