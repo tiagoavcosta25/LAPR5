6f7ee75db8bd4ff39f2a6c33f8e69bd49fa8d510
1190682 <1190682@isep.ipp.pt>
2022-01-02 22:36:46 +0000

    LAPR5-146 #node #implementation Node.js Post Map Class Development

---
+++ g4s-feed-module/src/mappers/PostMap.ts
@@ -0,0 +1,37 @@
+import { Mapper } from "../core/infra/Mapper";
+
+import { Document, Model } from 'mongoose';
+import { IPostPersistence } from '../dataschema/IPostPersistence';
+
+import IPostDTO from "../dto/IPostDTO";
+import { Post } from "../domain/post";
+
+import { UniqueEntityID } from "../core/domain/UniqueEntityID";
+
+export class PostMap extends Mapper<Post> {
+  
+  public static toDTO( post: Post): IPostDTO {
+    return {
+      id: post.id.toString(),
+      content: post.content,
+    } as IPostDTO;
+  }
+
+  public static toDomain (post: any | Model<IPostPersistence & Document> ): Post {
+    const postOrError = Post.create(
+      post,
+      new UniqueEntityID(post.domainId)
+    );
+
+    postOrError.isFailure ? console.log(postOrError.error) : '';
+
+    return postOrError.isSuccess ? postOrError.getValue() : null;
+  }
+
+  public static toPersistence (post: Post): any {
+    return {
+      domainId: post.id.toString(),
+      content: post.content
+    }
+  }
+}
