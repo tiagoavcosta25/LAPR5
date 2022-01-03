41a357f39fdc1041757f5d6c70310bff11571a6f
Pedro Santos <1190967@isep.ipp.pt>
2022-01-03 11:29:54 +0000

    LAPR5-118 #angular #implementation Angular Terms and Condition RGPD Legal Text Component Development

---
+++ g4s-spa-module/src/app/modules/general/components/terms-and-cond/terms-and-cond.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-cond',
+  templateUrl: './terms-and-cond.component.html',
+  styleUrls: ['./terms-and-cond.component.css']
+})
+export class TermsAndCondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
