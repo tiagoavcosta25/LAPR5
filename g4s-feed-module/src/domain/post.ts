2e1a9fd410c4f83525758b8881ae03cf7b539fa3
1190682 <1190682@isep.ipp.pt>
2022-01-07 18:54:19 +0000

    LAPR5-143 #node #implementation Node.js Post  Adjustments

--- g4s-feed-module/src/domain/post.ts
+++ g4s-feed-module/src/domain/post.ts
@@ -1,13 +1,15 @@
 import { AggregateRoot } from "../core/domain/AggregateRoot";
 import { UniqueEntityID } from "../core/domain/UniqueEntityID";
-import { Result } from "../core/logic/Result";
-import { Guard } from "../core/logic/Guard";
-import { PostId } from "./postId";
-import { PostContent } from "./postContent";
 
+import { Result } from "../core/logic/Result";
+import { PostId } from "./postId";
+
+import IPostDTO from "../dto/IPostDTO";
+import { PostContent } from "./postContent";
 
 interface PostProps {
   content: PostContent;
+  creatorId: string;
 }
 
 export class Post extends AggregateRoot<PostProps> {
@@ -15,35 +17,43 @@ export class Post extends AggregateRoot<PostProps> {
     return this._id;
   }
 
-  get userId (): PostId {
-    return PostId.caller(this.id);
+  get postId (): PostId {
+    return new PostId(this.postId.toValue());
   }
 
   get content (): PostContent {
     return this.props.content;
   }
 
+  set content ( value: PostContent) {
+    this.props.content = value;
+  }
+
+  get creatorId (): string {
+    return this.props.creatorId;
+  }
+
+  set creatorId ( value: string) {
+    this.props.creatorId = value;
+  }
+
   private constructor (props: PostProps, id?: UniqueEntityID) {
     super(props, id);
   }
 
-  public static create (props: PostProps, id?: UniqueEntityID): Result<Post> {
+  public static create (postDTO: IPostDTO, id?: UniqueEntityID): Result<Post> {
+    const content = postDTO.content;
+    const creatorId = postDTO.creatorId;
 
-    const guardedProps = [
-      { argument: props.content, argumentName: 'content' }
-    ];
-
-    const guardResult = Guard.againstNullOrUndefinedBulk(guardedProps);
-
-    if (!guardResult.succeeded) {
-      return Result.fail<Post>(guardResult.message)
-    }     
-    else {
-      const user = new Post({
-        ...props
-      }, id);
-
-      return Result.ok<Post>(user);
+    if (!!content === false || !!creatorId === false || content.length === 0) {
+      return Result.fail<Post>('Must provide a post content and creator')
+    } else {
+      const resContent = PostContent.create(content);
+      if(resContent.isSuccess){
+        const post = new Post({ content: resContent.getValue(), creatorId: creatorId }, id);
+        return Result.ok<Post>( post )
+      }
+      return Result.fail<Post>('Post content error')
     }
   }
 }
