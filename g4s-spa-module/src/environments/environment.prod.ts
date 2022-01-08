587428f0dcf7b18fafaa268cfa48f755f269b1a5
Pedro Santos <1190967@isep.ipp.pt>
2022-01-08 20:20:57 +0000

    LAPR5-143 #angular #implementation Angular Post Front End SPA Production Environment Changes

--- g4s-spa-module/src/environments/environment.prod.ts
+++ g4s-spa-module/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://socialnetworkapi51.azurewebsites.net/api',
-  aiUrl: 'https://socialnetworkaig51.westeurope.cloudapp.azure.com'
+  aiUrl: 'https://socialnetworkaig51.westeurope.cloudapp.azure.com',
+  feedUrl: 'http://localhost:3000/api'
 };
