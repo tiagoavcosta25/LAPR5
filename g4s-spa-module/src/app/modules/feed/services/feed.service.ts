0de12bade24cf70cc956dff4524063407980ee21
1190682 <1190682@isep.ipp.pt>
2022-01-08 20:05:29 +0000

    LAPR5-143 #angular #implementation Angular Post Front End SPA  Feed Servide and Node.Js Connection Implementation

--- g4s-spa-module/src/app/modules/feed/services/feed.service.ts
+++ g4s-spa-module/src/app/modules/feed/services/feed.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Post } from 'src/shared/models/feed/post.model';
+import { CreatingPost } from '../models/creating-post.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FeedService {
 
-  constructor() { }
+  postUrl: string = environment.feedUrl + '/post/';  // URL to web api
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(
+    private http: HttpClient) {
+  }
+
+  /** POST: create new post to the database */
+  createPost(post: CreatingPost): Observable<Post> {
+    console.log(this.postUrl);
+    console.log(post);
+    return this.http.post<Post>(this.postUrl, post, this.httpOptions).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  private handleError(err: HttpErrorResponse) {
+    console.error('An error occurred: ', err.error.errors.message);
+    if (err.error instanceof ErrorEvent) {
+      console.error('An error occurred: ', err.error.message);
+    }
+    else {
+      console.error(
+        `Web Api returned code ${err.status}, ` + ` Response body was: ${err.error}`
+      );
+    }
+    return throwError(() => err);
+  }
 }
