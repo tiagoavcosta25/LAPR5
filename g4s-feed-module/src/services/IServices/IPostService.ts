62a91b14e5cfc39e48f88dfd3ce7bb342a2b7582
1190682 <1190682@isep.ipp.pt>
2022-01-08 18:06:41 +0000

    LAPR5-143 #node #implementation Node.js Post Like and Dislike System Service  Implementation

--- g4s-feed-module/src/services/IServices/IPostService.ts
+++ g4s-feed-module/src/services/IServices/IPostService.ts
@@ -1,9 +1,12 @@
 import { Result } from "../../core/logic/Result";
 import IPostDTO from "../../dto/IPostDTO";
+import IReactionDTO from "../../dto/IReactionDTO";
 
 export default interface IPostService  {
   createPost(postDTO: IPostDTO): Promise<Result<IPostDTO>>;
   updatePost(postDTO: IPostDTO): Promise<Result<IPostDTO>>;
+  likePost(postDTO: IReactionDTO): Promise<Result<IPostDTO>>;
+  dislikePost(postDTO: IReactionDTO): Promise<Result<IPostDTO>>;
 
   getPost (postId: string): Promise<Result<IPostDTO>>;
 }
