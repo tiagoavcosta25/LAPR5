15f097ba1d71ecd23ea7522d04ed8b4081b0a134
1191460 <1191460@isep.ipp.pt>
2022-01-16 15:03:13 +0000

    LAPR5-153 #node #implementation Comments added and retrieve posts by user

--- g4s-spa-module/src/environments/environment.prod.ts
+++ g4s-spa-module/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://socialnetworkapi51.azurewebsites.net/api',
   aiUrl: 'https://socialnetworkaig51.westeurope.cloudapp.azure.com',
-  feedUrl: 'http://localhost:3000/api'
+  feedUrl: 'https://socialnetworkfeedapi51.azurewebsites.net/api'
 };
