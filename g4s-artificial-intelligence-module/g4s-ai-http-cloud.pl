0b494c51071e1493030115bd7cb323e5fb3713d0
Pedro Santos <1190967@isep.ipp.pt>
2022-01-21 02:20:19 +0000

    LAPR5-182 #ai #implementation Emotional State Changes Based on Relation and Suggested Groups Cloud Implementation

--- g4s-artificial-intelligence-module/g4s-ai-http-cloud.pl
+++ g4s-artificial-intelligence-module/g4s-ai-http-cloud.pl
@@ -27,6 +27,9 @@
 :- dynamic suggest_currentRoute/2.
 :- dynamic common_tags_users/2.
 :- dynamic aStar_orderedList/1.
+:- dynamic occ/7.
+:- dynamic fear/2.
+:- dynamic hope/2.
 
 
 % HTTP Server setup at 'Port'
@@ -643,3 +646,116 @@ aStar_getStrengthListByFriendsList(1, PlayerId, [FriendId|FriendList], [FirstMul
     getMulticriteria(FirstStrength, FirstRelStrength, FirstMulti),
     getMulticriteria(SecondStrength, SecondRelStrength, SecondMulti),
     aStar_getStrengthListByFriendsList(1, PlayerId, FriendList, StrengthList).
+
+
+%======== Emotion Relation Variation (HTTP) ========%
+
+:- http_handler('/api/emotion-relation', emotion_relationCompute, []).
+
+emotion_relationCompute(Request) :-
+	cors_enable(Request, [methods([get])]),
+    emotion_relationPrepare(Request, NewJoy, _),
+	prolog_to_json(NewJoy, JSONObject),
+    reply_json(JSONObject, [json_object(dict)]).
+
+emotion_relationPrepare(Request, NewJoy, NewAnguish) :-
+    http_parameters(Request, [emailPlayer(EmailPlayer, [string]), value(Value, [integer])]),
+	addPlayers(),
+	addConnections(),
+	getPlayerName(EmailPlayer, PlayerName),
+        node(PlayerId, PlayerName, _),
+	emotion_relationChange(PlayerId, Value, NewJoy, NewAnguish),
+	retractall(connection(_,_,_,_)),
+	retractall(node(_,_,_)).
+
+%======== Emotion Suggested Variation (HTTP) ========%
+
+:- http_handler('/api/emotion-suggested', emotion_suggestedCompute, []).
+
+emotion_suggestedCompute(Request) :-
+	cors_enable(Request, [methods([get])]),
+    emotion_suggestedPrepare(Request, NewHope, _, _, _),
+	prolog_to_json(NewHope, JSONObject),
+    reply_json(JSONObject, [json_object(dict)]).
+
+emotion_suggestedPrepare(Request, NewHope, NewDeception, NewFear, NewRelief) :-
+    http_parameters(Request, [emailPlayer(EmailPlayer, [string]), tags(Tags, [string])]),
+	addPlayers(),
+	addConnections(),
+	getPlayerName(EmailPlayer, PlayerName),
+        node(PlayerId, PlayerName, _),
+	emotion_groupSuggestion(PlayerId, Tags, NewHope, NewDeception, NewFear, NewRelief),
+	retractall(connection(_,_,_,_)),
+	retractall(node(_,_,_)).
+
+%======== Emotion Variation (Core) ========%
+
+emotion_increase(PreviousValue, Value, Saturation, Return):-
+    ((Value < Saturation, !, Min is Value);
+    (!, Min is Saturation)),
+    Div is Min / Saturation,
+    Return is PreviousValue + (1 - PreviousValue) * Div.
+
+emotion_decrease(PreviousValue, Value, Saturation, Return):-
+    ((Value < Saturation, !, Min is Value);
+    (!, Min is Saturation)),
+    Div is 1 - (Min / Saturation),
+    Return is PreviousValue * Div.
+
+emotion_relationChange(PlayerId, Value, NewJoy, NewAnguish):-
+    occ(PlayerId, Joy, Anguish, _, _, _, _),
+    ((Value > 0, !,
+     emotion_increase(Joy, Value, 200, NewJoy),
+     emotion_decrease(Anguish, Value, 200, NewAnguish));
+    (Value < 0, !,
+    emotion_increase(Anguish, -Value, 200, NewAnguish),
+    emotion_decrease(Joy, -Value, 200, NewJoy))),
+    retract(occ(PlayerId, Joy, Anguish, Hope, Deception, Fear, Relief)),
+    asserta(occ(PlayerId, NewJoy, NewAnguish, Hope, Deception, Fear, Relief)).
+
+emotion_groupSuggestion(PlayerId, TagList, NewHope, NewDeception, NewFear, NewRelief):-
+    suggest_playerGroups(PlayerId, TagList, SuggestedGroup),
+    emotion_checkHope(PlayerId, SuggestedGroup, NewHope, NewDeception),
+    emotion_checkFear(PlayerId, SuggestedGroup, NewFear, NewRelief),
+    retract(occ(PlayerId, Joy, Anguish, _, _, _, _)),
+    asserta(occ(PlayerId, Joy, Anguish, NewHope, NewDeception, NewFear, NewRelief)).
+
+emotion_checkHope(PlayerId, SuggestedGroup, NewHope, NewDeception):-
+    emotion_countHope(PlayerId, SuggestedGroup, 0, Counter),
+    occ(PlayerId, _, _, Hope, Deception, _, _),
+    length(SuggestedGroup, Length),
+    ((Counter > 0, !,
+      emotion_increase(Hope, Counter, Length, NewHope),
+     emotion_decrease(Deception, Counter, Length, NewDeception));
+    (emotion_increase(Deception, Counter, Length, NewDeception),
+    emotion_decrease(Hope, Counter, Length, NewHope))).
+
+emotion_countHope(_, [], Counter, Return):- Return is Counter.
+emotion_countHope(PlayerId, [H | Group], Counter, Return):-
+    hope(PlayerId, H),
+    !, Counter1 is Counter + 1,
+    emotion_countHope(PlayerId, Group, Counter1, Return).
+emotion_countHope(PlayerId, [_ | Group], Counter, Return):-
+    !,emotion_countHope(PlayerId, Group, Counter, Return).
+
+
+
+emotion_checkFear(PlayerId, SuggestedGroup, NewFear, NewRelief):-
+    emotion_countFear(PlayerId, SuggestedGroup, 0, Counter),
+    occ(PlayerId, _, _, _, _, Fear, Relief),
+    length(SuggestedGroup, Length),
+    ((Counter > 0, !,
+      emotion_increase(Fear, Counter, Length, NewFear),
+     emotion_decrease(Relief, Counter, Length, NewRelief));
+    (emotion_increase(Relief, Counter, Length, NewRelief),
+    emotion_decrease(Fear, Counter, Length, NewFear))).
+
+emotion_countFear(_, [], Counter, Return):- Return is Counter.
+emotion_countFear(PlayerId, [H | Group], Counter, Return):-
+    fear(PlayerId, H),
+    !, Counter1 is Counter + 1,
+    emotion_countFear(PlayerId, Group, Counter1, Return).
+emotion_countFear(PlayerId, [_ | Group], Counter, Return):-
+    !,emotion_countFear(PlayerId, Group, Counter, Return).
+
+suggest_playerGroups(_, _, [3,6, 4, 5]).
