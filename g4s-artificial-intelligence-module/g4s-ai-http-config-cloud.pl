1de9f4ca99fa92da0379fde03d38bd18dcb98ecf
1191460 <1191460@isep.ipp.pt>
2022-01-21 16:50:22 +0000

    LAPR5-179 #ai #implementation gateway changes

--- g4s-artificial-intelligence-module/g4s-ai-http-config-cloud.pl
+++ g4s-artificial-intelligence-module/g4s-ai-http-config-cloud.pl
@@ -1,8 +1,8 @@
 server_port(443). % Server port
 module_socialnetwork_host('localhost'). % Host who runs the SocialNetwork module
 module_socialnetwork_port(5000).
-api_url("https://socialnetworkapi51.azurewebsites.net/api/").
-players_url("https://socialnetworkapi51.azurewebsites.net/api/players/").
-connections_url("https://socialnetworkapi51.azurewebsites.net/api/connections/").
-dcalc_url("https://socialnetworkfeedapi51.azurewebsites.net/api/post/dcalc/").
+api_url("https://socialnetworkgatewayapi51.azurewebsites.net/apisocial/").
+players_url("https://socialnetworkgatewayapi51.azurewebsites.net/apisocial/players/").
+connections_url("https://socialnetworkgatewayapi51.azurewebsites.net/apisocial/connections/").
+dcalc_url("https://socialnetworkgatewayapi51.azurewebsites.net/apifeed/post/dcalc/").
 synonym('c#', csharp).
