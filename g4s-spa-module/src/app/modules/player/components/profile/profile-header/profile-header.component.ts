ddccb8fb67e8e15413fd903b8ff1becf31fa3bc7
1191460 <1191460@isep.ipp.pt>
2022-01-23 09:14:05 +0000

    LAPR5-179 #ai #implementation hotfix

--- g4s-spa-module/src/app/modules/player/components/profile/profile-header/profile-header.component.ts
+++ g4s-spa-module/src/app/modules/player/components/profile/profile-header/profile-header.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ConnectionService } from 'src/app/modules/connection/services/connection.service';
 import { RequestService } from 'src/app/modules/request/services/request.service';
+import { CreatingDirectRequest } from 'src/shared/models/requests/creating-direct-request.model';
+import { DirectRequest } from 'src/shared/models/requests/direct-request.model';
 import { DobPlayer } from '../../../models/dob-player.model copy';
 import { PlayerService } from '../../../services/player.service';
 
@@ -21,6 +23,8 @@ export class ProfileHeaderComponent implements OnInit{
 
   currentPlayer: string;
 
+  currentPlayerObject: DobPlayer;
+
   userEmail: string;
 
   player: DobPlayer;
@@ -30,6 +34,12 @@ export class ProfileHeaderComponent implements OnInit{
   isFriend: boolean;
 
   currentTab: string;
+
+  sendFriendRequest: boolean = false;
+
+  tags: string[] = [];
+
+  r: CreatingDirectRequest = new CreatingDirectRequest;
 
   constructor(private activatedRoute: ActivatedRoute,
     private pService: PlayerService,
@@ -43,6 +53,7 @@ export class ProfileHeaderComponent implements OnInit{
     $('[data-bs-toggle="tooltip"]').tooltip();
     this.checkCurrentTab();
     this.currentPlayer = localStorage.getItem("currentPlayer")!;
+    this.getCurrentPlayer();
     this.activatedRoute.params.subscribe(params => {
       this.userEmail = params['email'];
       if(this.userEmail != this.currentPlayer) {
@@ -69,6 +80,18 @@ export class ProfileHeaderComponent implements OnInit{
       if(!this.ownProfile) {
         this.getFriends();
       }
+      this.spinner.hide();
+    },
+      error: _error => {
+        this.spinner.hide();
+      }
+    });
+  }
+
+  getCurrentPlayer(): void {
+    this.spinner.show();
+    this.pService.getOnlyPlayerByEmail(this.currentPlayer).subscribe({ next: data => {
+      this.currentPlayerObject = data;
       this.spinner.hide();
     },
       error: _error => {
@@ -183,5 +206,65 @@ export class ProfileHeaderComponent implements OnInit{
         return "👾";
       }
     }
+  }
+
+  addTag() {
+    let el = document.getElementById('tagvalue');
+    let val = "";
+    if(el != undefined) {
+      val = ((<HTMLInputElement>el).value);
+      (<HTMLInputElement>el).value = "";
+    }
+    if(this.tags.indexOf(val) < 0) {
+      this.tags.push(val);
+    }
+  }
+
+  removeTag(tag: string) {
+    let index = this.tags.findIndex( t => t == tag);
+    if (index != -1) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  createDirectRequest() {
+    let el = document.getElementById("messageToPlayer");
+    let messageToPlayer = "";
+    let strength: number = 0;
+    if(el != undefined) {
+      messageToPlayer = ((<HTMLInputElement>el).value);
+      (<HTMLInputElement>el).value = "";
+    }
+    let el2 = document.getElementById("connectionStrength");
+    if(el2 != undefined) {
+      strength =+ ((<HTMLInputElement>el2).value);
+      (<HTMLInputElement>el2).value = "";
+    }
+
+    this.r.player = this.currentPlayerObject.id;
+    this.r.target = this.player.id;
+    this.r.playerToTargetMessage = messageToPlayer;
+    this.r.strength = strength;
+    this.r.tags = this.tags;
+  }
+
+  sendRequest(): void {
+    this.createDirectRequest();
+    this.spinner.show();
+    this.rService.sendDirectRequest(this.r)
+    .subscribe({ next: data => {
+      if(data) {
+        this.r = new CreatingDirectRequest;
+      }
+      this.sendFriendRequest = false;
+      this.spinner.hide();
+      this.ngOnInit();
+    },
+      error: _error => {
+        this.sendFriendRequest = false;
+        this.r = new CreatingDirectRequest;
+        this.spinner.hide();
+      }
+    });
   }
 }
